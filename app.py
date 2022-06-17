439e3f908ae052566c1065300ed7f0599077144e
Mario Kozic <mkozic89@gmail.com>
2022-06-16 22:48:34 -0400

    testing completed

--- app.py
+++ app.py
@@ -4,16 +4,6 @@ from difflib import get_close_matches
 data = json.load(open('data.json'))
 
 
-# variables
-
-word = input('Enter word: ')
-output = translate(word)
-
-if type(output) == list:
-    for item in output:
-        print(item)
-else:
-    print(output)
 
 # setting up out function
 
@@ -26,9 +16,20 @@ def translate(w):
         yesOrNo = input('Did you mean %s instead? Enter Y if yes, or N if no: ' % get_close_matches(w, data.keys())[0])
         if yesOrNo == 'Y':
             return  data[get_close_matches(w, data.keys())[0]]
-        elif yesOrNo == 'N'
+        elif yesOrNo == 'N':
             return "The word doesn't exist. Please double check it."
         else:
             return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
+
+# variables
+
+word = input('Enter word: ')
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
