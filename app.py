5d5e12ab53b86e568b570ed637c01e773b2f734f
Mario Kozic <mkozic89@gmail.com>
2022-06-16 22:42:59 -0400

    testing

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ data = json.load(open('data.json'))
 word = input('Enter word: ')
 output = translate(word)
 
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
+
 # setting up out function
 
 def translate(w):
@@ -20,3 +26,9 @@ def translate(w):
         yesOrNo = input('Did you mean %s instead? Enter Y if yes, or N if no: ' % get_close_matches(w, data.keys())[0])
         if yesOrNo == 'Y':
             return  data[get_close_matches(w, data.keys())[0]]
+        elif yesOrNo == 'N'
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
